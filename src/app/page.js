e68cd62e972d411dafd89a7555f723f2b46d593d
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2024-03-18 00:25:37 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,204 @@
-import Image from "next/image";
+import Chat from '@/components/Chat';
+import Navbar from '@/components/Navbar';
+import { Bilbo_Swash_Caps, Poppins, Yeseva_One } from 'next/font/google';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const bilboSwashCaps = Bilbo_Swash_Caps({
+  weight: ['400'],
+  style: ['normal'],
+  subsets: ['latin'],
+});
+
+const yesevaOne = Yeseva_One({
+  weight: ['400'],
+  style: ['normal'],
+  subsets: ['latin'],
+});
+
+const poppins = Poppins({
+  weight: ['300', '400', '500', '600', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+});
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='relative'>
+      <Chat />
+      <header
+        className='h-screen relative bg-cover bg-center text-white'
+        style={{
+          backgroundImage:
+            "linear-gradient(to top, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/images/background.jpg')",
+        }}
+      >
+        <Navbar />
+        <section className='h-screen flex justify-center items-center text-center'>
+          <div>
+            <h1 className='text-8xl italic' style={bilboSwashCaps.style}>
+              Pt Saputra Tenda Sejahtera
+            </h1>
+            <p className='mt-6 text-xl' style={poppins.style}>
+              Konveksi pembuat perlengkapan kain untuk dekorasi tenda, hotel, dan kantor.
+            </p>
+          </div>
+        </section>
+      </header>
+      <section className='my-20 flex justify-center gap-x-11' style={poppins.style}>
+        <div>
+          <h3 className='text-3xl font-semibold text-[#576250]' style={yesevaOne.style}>
+            Best Seller
+          </h3>
+          <p className='text-[#B8C1B2]'>Our best selling products</p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <div className='flex gap-x-4 text-[#90998B]'>
+          <div className='card w-fit p-3 bg-white text-center drop-shadow-lg rounded-lg'>
+            <Image src='/images/photography.png' height={223} width={210} alt='photography' className='rounded-lg' />
+            <p className='mt-4'>Rumbai Tenda</p>
+          </div>
+          <div className='card w-fit p-3 bg-white text-center drop-shadow-lg rounded-lg'>
+            <Image src='/images/ceremony.png' height={223} width={210} alt='photography' className='rounded-lg' />
+            <p className='mt-4'>Plafon Datar</p>
+          </div>
+          <div className='card w-fit p-3 bg-white text-center drop-shadow-lg rounded-lg'>
+            <Image src='/images/wedding-design.png' height={223} width={210} alt='photography' className='rounded-lg' />
+            <p className='mt-4'>Set Dekorasi</p>
+          </div>
+        </div>
+      </section>
+      <section className='my-20' style={poppins.style}>
+        <div className='text-center'>
+          <h3 className='text-3xl font-semibold text-[#576250]' style={yesevaOne.style}>
+            Categories
+          </h3>
+          <p className='text-[#B8C1B2]'>Our Category Products</p>
+        </div>
+        <div className='mt-6 w-fit container mx-auto grid grid-cols-4 gap-3 justify-items-center'>
+          <div className='card-category relative'>
+            <Image
+              src='/images/category.png'
+              height={153}
+              width={248}
+              alt='category'
+              className='rounded-lg drop-shadow-lg'
+            />
+            <p className='absolute bottom-2 left-3 text-white text-sm'>Sarung Kursi</p>
+          </div>
+          <div className='card-category relative'>
+            <Image
+              src='/images/category.png'
+              height={153}
+              width={248}
+              alt='category'
+              className='rounded-lg drop-shadow-lg'
+            />
+            <p className='absolute bottom-2 left-3 text-white text-sm'>Rumbai / Poni Tenda</p>
+          </div>
+          <div className='card-category relative'>
+            <Image
+              src='/images/category.png'
+              height={153}
+              width={248}
+              alt='category'
+              className='rounded-lg drop-shadow-lg'
+            />
+            <p className='absolute bottom-2 left-3 text-white text-sm'>Plafon Dekorasi Tenda</p>
+          </div>
+          <div className='card-category relative'>
+            <Image
+              src='/images/category.png'
+              height={153}
+              width={248}
+              alt='category'
+              className='rounded-lg drop-shadow-lg'
+            />
+            <p className='absolute bottom-2 left-3 text-white text-sm'>Cover Meja</p>
+          </div>
+          <div className='card-category relative'>
+            <Image
+              src='/images/category.png'
+              height={153}
+              width={248}
+              alt='category'
+              className='rounded-lg drop-shadow-lg'
+            />
+            <p className='absolute bottom-2 left-3 text-white text-sm'>Kain Pelamin</p>
+          </div>
+          <div className='card-category relative'>
+            <Image
+              src='/images/category.png'
+              height={153}
+              width={248}
+              alt='category'
+              className='rounded-lg drop-shadow-lg'
+            />
+            <p className='absolute bottom-2 left-3 text-white text-sm'>Background Dinding</p>
+          </div>
+          <div className='card-category relative'>
+            <Image
+              src='/images/category.png'
+              height={153}
+              width={248}
+              alt='category'
+              className='rounded-lg drop-shadow-lg'
+            />
+            <p className='absolute bottom-2 left-3 text-white text-sm'>Dekorasi Tenda Lengkap</p>
+          </div>
+          <div className='card-category relative'>
+            <Image
+              src='/images/category.png'
+              height={153}
+              width={248}
+              alt='category'
+              className='rounded-lg drop-shadow-lg'
+            />
+            <p className='absolute bottom-2 left-3 text-white text-sm'>Preorder</p>
+          </div>
+        </div>
+      </section>
+      <section className='w-full py-12 bg-[#585B56]' style={poppins.style}>
+        <div className='text-white text-center'>
+          <h3 className='text-3xl font-semibold' style={yesevaOne.style}>
+            Testimonials
+          </h3>
+          <p className='text-[#B8C1B2]'>from happy, delighted couples</p>
+        </div>
+        <div className='flex justify-center gap-x-4'>
+          <p className='mt-8 text-9xl text-[#464C43]' style={yesevaOne.style}>
+            “
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p className='w-1/2 my-12 text-base 2 text-center text-white'>
+            Lorem ipsum sit ac suspendisse adipiscing eget aliquam fermentum rutrum tempus aenean. Suspendisse nascetur
+            vulputate viverra varius eu ullamcorper. Cursus pellentesque erat eget eget ullamcorper id in. Nullam
+            scelerisque varius viverra augue quis. Laoreet eget curabitur vitae sed at facilisis purus convallis
+            imperdiet. Penatibus auctor diam vivamus amet a vehicula ultrices. Volutpat arcu in amet praesent nisl
+            mattis etiam cras orci. Ut urna elementum sapien orci lacus!
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+          <p className='mt-8 text-9xl text-[#464C43]' style={yesevaOne.style}>
+            ”
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        </div>
+        <div className='flex justify-center'>
+          <div className='text-white flex gap-x-4'>
+            <div className='h-[2px] w-8 my-auto bg-white'></div>
+            <p className='my-auto text-lg '>Mahdy Mubasyir </p>
+            <div className='h-[2px] w-8 my-auto bg-white'></div>
+          </div>
+        </div>
+      </section>
+      <section className='py-12 flex justify-center'>
+        <iframe
+          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.6908676935172!2d106.9467042748299!3d-6.3042859936849105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69928569a1c79d%3A0x9eb3424e56a9e37f!2sSAPUTRA%20TENDA!5e0!3m2!1sid!2sid!4v1710692251259!5m2!1sid!2sid'
+          className='w-full h-[500px] lg:w-[1300px] lg:h-[450px]'
+          // width='600'
+          // height='450'
+          // style='border:0;'
+          allowfullscreen=''
+          loading='lazy'
+          referrerpolicy='no-referrer-when-downgrade'
+        ></iframe>
+      </section>
     </main>
   );
 }
---
+++ src/components/Chat.js
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+export default function Chat() {
+  return (
+    <Link href='https://wa.me/1234567890' className='fixed content-center bottom-5 right-10 cursor-pointer z-10'>
+      {/* <span className='text-black'>Hubungi Kami</span> */}
+      {/* <br /> */}
+      <Image src='/images/wa.png' height={50} width={50} alt='WhatsApp Chat' className='mx-auto' />
+    </Link>
+  );
+}
