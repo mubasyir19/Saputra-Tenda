36f5dea4d0ebefd7eb5ac4ed538b5e9c1b36a46f
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2024-04-20 19:55:00 +0700

    update(menu): set font menu group route

--- src/app/(menu)/layout.js
+++ src/app/(menu)/layout.js
@@ -1,17 +1,24 @@
 import '../globals.css';
 import Chat from '@/components/Chat';
 import Footer from '@/components/Footer';
+import { Poppins } from 'next/font/google';
 
 export const metadata = {
   title: 'PT Saputra Tenda Sejahtera',
   description: 'Konveksi pembuat perlengkapan kain untuk dekorasi tenda, hotel, dan kantor.',
 };
 
+const poppins = Poppins({
+  weight: ['300', '400', '500', '600', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+});
+
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body>
-        <main className='relative'>
+        <main className='relative' style={poppins.style}>
           <Chat />
           {children}
           <Footer />
